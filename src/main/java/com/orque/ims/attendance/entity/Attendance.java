44b0d621df4f2e16c489e329c5a5a7e2b9182378
ayukth63 <krishnaprasadsm63@gmail.com>
2026-03-16 20:04:42 +0530

    entity change

--- src/main/java/com/orque/ims/attendance/entity/Attendance.java
+++ src/main/java/com/orque/ims/attendance/entity/Attendance.java
@@ -1,27 +1,32 @@
 package com.orque.ims.attendance.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "attendance_records")
 public class Attendance {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(name = "employee_id", nullable = false)
     private String employeeId;
-    
+
     @Column(name = "employee_name", nullable = false)
     private String employeeName;
-    
+
     @Column(name = "record_date", nullable = false)
     private LocalDate date;
-    
+
     @Column(nullable = false)
     private String status; // 'Present' or 'Leave'
 }
